99d222acb91a80cff6c78d0331c74370776e5b7b
erick <ericken15@ciencias.unam.mx>
2018-07-02 18:48:14 -0500

    layout view

---
+++ Spotify/app/src/main/java/com/example/erick/spotify/Spoty.java
@@ -0,0 +1,13 @@
+package com.example.erick.spotify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Spoty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spoty);
+    }
+}
