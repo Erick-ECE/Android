546b66d2b3048f15571e91558a012384b40cb7e5
Erick-ECE <ericken15@ciencias.unam.mx>
2018-07-17 22:54:59 -0500

    terminado

--- SharedPref/app/src/main/java/com/example/erick/sharedpref/MainActivity.java
+++ SharedPref/app/src/main/java/com/example/erick/sharedpref/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.erick.sharedpref;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
@@ -15,83 +16,60 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1;
-    Button b2;
-    Switch s1;
-    EditText e1;
-    TextView t1;
-    LinearLayout ll;
-    public static final String SHARED_PREFS = "Shared_Prefs";
-    public static final String COLOR = "color";
-    public static final String SWITCH1 = "switch1";
-    int color;
-    int color2;
-    boolean sw_v;
-
+    public static final Random _RAND = new Random();
+    Button btn_guardar,btn_randColor;
+    LinearLayout lo;
+    Switch stch1;
+    int new_color;
+    int saved_color;
+    boolean switch_state;
+    private static final String _MIS_PREFERENCIAS = "Mis_Preferencias";
+    private static final String _BACKGROUND = "Background";
+    private static final String _MARCADOR = "Marcador";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LinearLayout ll = findViewById(R.id.ly1);
-        Switch s1 = findViewById(R.id.sw1);
-        btn1();
-        btn2();
-        load();
-        update();
-
+        btn_randColor = findViewById(R.id.btn_randomColors);
+        btn_guardar = findViewById(R.id.btn_save);
+        lo = findViewById(R.id.ly1);
+        stch1 = findViewById(R.id.sw1);
+        loadPreferences();
 
     }
 
-    private void update() {
-        ll.setBackgroundColor(color2);
-        s1.setChecked(sw_v);
-
+    public void setRandBackground(View view){
+       new_color =  Color.argb(255,
+               _RAND.nextInt(255),
+               _RAND.nextInt(255),
+               _RAND.nextInt(255));
+        lo.setBackgroundColor(new_color);
     }
 
-    private void load() {
-        SharedPreferences sp = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
-        color2 = sp.getInt(COLOR,0);
-        sw_v = sp.getBoolean(SWITCH1,false);
 
+
+    public void  guardarPreferencias(View view){
+        //getSharedPreferences("identificado de la coleccion", "modo de acceso")
+        // ya solo se usa el modo de acceso privado
+        SharedPreferences prefs = getSharedPreferences(_MIS_PREFERENCIAS, Context.MODE_PRIVATE);
+        // se crea un editor para poder actualizar/insertar un valor a la coleccion
+        SharedPreferences.Editor editor= prefs.edit();
+        editor.putInt(_BACKGROUND,new_color);
+        editor.putBoolean(_MARCADOR,stch1.isChecked());// isChecked() regresa un booleano con el estado del switch
+        editor.commit(); // se llama al commit para confirmar los cambios
     }
 
-    private void btn1(){
-        Button b1 = findViewById(R.id.btn1);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Random rnd = new Random();
-                int color = Color.argb(225,
-                        rnd.nextInt(256),
-                        rnd.nextInt(256),
-                        rnd.nextInt(256));
-                ll.setBackgroundColor(color);
-            }
-        });
+    private void loadPreferences(){
+        SharedPreferences prefs = getSharedPreferences(_MIS_PREFERENCIAS,MODE_PRIVATE);
+        //salvamos en una variable el valor previamente salvado en el identificador "_BACKGROUND"
+        //en caso de no existir dicho valor, se pasa el valor por defecto '0'
+        saved_color = prefs.getInt(_BACKGROUND,0);
+        switch_state = prefs.getBoolean(_MARCADOR,false);//de igual manera secede con getBoolean()
+        lo.setBackgroundColor(saved_color);
+        stch1.setChecked(switch_state);
     }
-
-    private void btn2(){
-        Button b2 = findViewById(R.id.btn2);
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                guardar();
-            }
-        });
-    }
-
-    private void guardar() {
-        SharedPreferences sp = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putInt(COLOR,color);
-        editor.putBoolean(SWITCH1,s1.isChecked());
-        editor.apply();
-        Toast.makeText(this,"sp guardadas",Toast.LENGTH_LONG);
-
-    }
-
 
 
 }
